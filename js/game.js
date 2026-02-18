2a5256daf8d6e9955568e8289c980cc5e124b0a3
MDOB93 <maik.dobosch@posteo.de>
2026-02-18 18:12:06 +0100

    feat: add all images and classes for world, character, chicken | update index.html and game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,11 @@
-//gamecode
+let canvas;
+let ctx;
+let world = new World();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log('My Character is', world.character);
+    
+}
---
+++ models/chicken.class.js
@@ -0,0 +1,3 @@
+class Chicken extends MovableObject {
+    img = '../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png'
+}
