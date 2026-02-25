f8b2afb43017e2f4f1f1e7e32ea7226745123288
MDOB93 <maik.dobosch@posteo.de>
2026-02-25 22:32:17 +0100

    feat: add more and other enemies

--- models/small.chicken.class.js
+++ models/small.chicken.class.js
@@ -1,16 +1,19 @@
-class Chicken extends MovableObject {
+class SmallChicken extends MovableObject {
     imagesWalking = [
-        './img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        './img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        './img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+        './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        './img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
     ];
     
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
-        this.x = 1440 + Math.random() * 1000;
+        this.x = 1350 + Math.random() * 1000;
+        this.y = 370;
+        this.height = 75;
+        this.width = 50;
         this.loadImages(this.imagesWalking);
 
-        this.speed = 0.5 + Math.random() * 0.25;
+        this.speed = 0.8 + Math.random() * 0.25;
 
         this.animate();
     }
